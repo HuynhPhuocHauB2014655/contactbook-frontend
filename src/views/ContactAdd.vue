8e025b2224733aad864692addeb33a785c0597de
HuynhPhuocHauB2014655 <haub2014655@student.ctu.edu.vn>
2024-03-09 09:19:50 +0700

    cai dat thêm contact(Quên commit)

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="page">
+        <h4>Thêm Liên Hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="addContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: { name: null, email: null, address: null, phone: null, favorite: false },
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ được thêm thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+}
+</script>
